8833fcdadfac366d232d946c68da43ea046c1885
Michal Hoftich <michal.h21@gmail.com>
2018-01-12 13:16:56 +0100

    more translations

--- trans/eng.lua
+++ trans/eng.lua
@@ -1,4 +1,11 @@
 return {
- ["Hledat na webu knihovny"] = "Search on the library site",
- ["Aktuality"] = "News"
+ ["Hledat na webu knihovny"] = "Search on the library site"
+ ,["Aktuality"] = "News"
+ ,[' (<a href="archiv.html">archiv</a>)'] = ' (<a href="archive-en.html">archive</a>)'    
+ ,[ "Katalog"]= "Catalog"
+ ,[ "https://ckis.cuni.cz/F/"] =  "https://ckis.cuni.cz/F/?CON_LNG=ENG"
+ ,["Klíčová slova:"] = "Keywords:"
+ ,["Vyhledat v: "] = "Search in: "
+ ,[ "Všechna pole"] = "All fields"
+ -- ,[
 }

05ba3617940295d36eed9ebbafff5ef5b4c83bf8
Michal Hoftich <michal.h21@gmail.com>
2018-07-30 12:26:01 +0200

    added lua time library

---
+++ lib/lua-time/__meta.lua
@@ -0,0 +1,10 @@
+return {
+  name = "time",
+  version = "1.1",
+  require = {
+    luajit = "2.0",
+  },
+  license = "MIT <http://opensource.org/licenses/MIT>",
+  homepage = "http://scilua.org/time.html",
+  description = "dates and periods in Lua",
+}

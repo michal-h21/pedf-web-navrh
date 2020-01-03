f2a04c5379803b514aacb74b3ab4d39718980a8c
Michal Hoftich <michal.h21@gmail.com>
2020-01-03 11:46:57 +0100

    localised page address

--- templates/base.lua
+++ templates/base.lua
@@ -160,7 +160,7 @@ local function template(data)
         -- class="row",
         h.span {class="logo", "&nbsp;"},
         mainmenu(data.menuitems),
-        h.span{ a{href=T "/index-en.html",h.img{src=T "/img/gb.svg", alt=T "English version", style="width:1em;"}}} -- odkaz na anglickou verzi stránek
+        h.span{ a{href=(data.altlang or T "/index-en.html"),h.img{src=T "/img/gb.svg", alt=T "English version", style="width:1em;"}}} -- odkaz na anglickou verzi stránek
         -- }},
       }},
       -- row{

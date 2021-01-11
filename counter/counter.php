be6b9f379dd57dad90f3ae782000191f01d005a8
Michal Hoftich <michal.h21@gmail.com>
2021-01-12 00:39:00 +0100

    vycisteni komentaru

--- counter/counter.php
+++ counter/counter.php
@@ -1,5 +1,6 @@
 <?php
-/* var_export($_GET); */
+// simple counter
+// the JS part is from Goatcounter
 
 $ipaddress = $_SERVER['REMOTE_ADDR'];
 $timestamp = date('Y/m/d h:i:s');
@@ -11,12 +12,5 @@ $browser = $_SERVER['HTTP_USER_AGENT'];
 
 $msg = md5($ipaddress) . "\t$timestamp\t$page\t$referrer\t$browser\t$dimensions\t$bot\n";
 
-echo($msg);
 $result = file_put_contents(getcwd(). "/counter.txt",$msg, FILE_APPEND | LOCK_EX) or print_r(error_get_last());;
-/* if($result===false){ */
-/* echo "něco je špatně<br />"; */
-/* }else{ */
-/* echo "v pohodě?"; */
-/* } */
-/* echo("what?"); */
 ?>
